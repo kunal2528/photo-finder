f6a43d107deeb266f73b9c1f875f01568a9467ee
kunal2528 <kunalmewara01@gmail.com>
2023-02-06 14:47:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const fetchImages = () => {
+    fetch(`https://api.unsplash.com/search/photos?client_id=1QY0dySjDyA9BfP6E0Cx9xCtyPq9Jl8L3EHljpD__LU&query=${search}&orientation=squarish&per_page=20`)
+    .then(res=>res.json())
+    .then(data => {
+      setResults(data.results)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='myDiv'>
+        <TextField label="Search here" variant="outlined" value={search} onChange={(e) => setSearch(e.target.value)} size="small" style={{width: '50%'}} />
+        <Button style={{marginLeft: '20px', padding: '7px 10px'}} variant="contained" onClick={fetchImages}>Search</Button>
+      </div>
+      <div className='gallery'>
+        {
+          results.map((item) => 
+            <img className='item' key={item.id} src={item.urls.regular} alt={item.alt_description} />
+          )
+        }
+      </div>
     </div>
+    
   );
 }
 
